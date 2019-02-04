ba94dbd039ad8a616e2ce0be1ecad0dfc11b696f
ybod <yuriibodarev@gmail.com>
2019-02-04 18:05:54 +0200

    Update initialization parameters

--- lib/tomloc.ex
+++ lib/tomloc.ex
@@ -1,92 +1,103 @@
 defmodule Tomloc do
-  use GenServer
+  @moduledoc """
+  Tomloc GenServer
+  """
 
-  alias Tomloc.StringParser
-  alias Tomloc.Core
+  @doc false
+  defmacro __using__(otp_app: otp_app) do
+    quote bind_quoted: [otp_app: otp_app] do
+      use GenServer
 
-  # Server (callbacks)
-  @impl true
-  def init(params) do
-    table = :ets.new(__MODULE__, [:set, :protected, {:read_concurrency, true}])
+      alias Tomloc.StringParser
+      alias Tomloc.Core
 
-    send(self(), {:init, table, params})
-    Process.register(self(), __MODULE__)
+      @otp_app otp_app
 
-    {:ok, table}
-  end
+      # Server (callbacks)
+      @impl true
+      def init(params) do
+        table = :ets.new(__MODULE__, [:set, :protected, {:read_concurrency, true}])
 
-  @impl true
-  def handle_info({:init, table, params}, _) do
-    tomloc_dir = Keyword.get(params, :localizations_dir) || Application.get_env(:tomloc, :tomloc_dir)
+        send(self(), {:init, table, params})
+        Process.register(self(), __MODULE__)
 
-    tomlock_files =
-      [:code.priv_dir(:tomloc), tomloc_dir, "*.toml"]
-      |> Path.join()
-      |> Path.wildcard()
+        {:ok, table}
+      end
 
-    Enum.each(tomlock_files, fn tomloc_file ->
-      basename = Path.basename(tomloc_file, ".toml")
-      {:ok, tomloc} = Toml.decode_file(tomloc_file)
+      @impl true
+      def handle_info({:init, table, params}, _) do
+        tomloc_dir = Keyword.get(params, :localizations_dir) || Application.get_env(:tomloc, :tomloc_dir, "tomloc")
 
-      process_tomloc(table, basename, Map.to_list(tomloc))
-    end)
+        tomlock_files =
+          [Application.app_dir(@otp_app, "priv"), tomloc_dir, "*.toml"]
+          |> Path.join()
+          |> Path.wildcard()
 
-    {:noreply, table, :hibernate}
-  end
+        Enum.each(tomlock_files, fn tomloc_file ->
+          basename = Path.basename(tomloc_file, ".toml")
+          {:ok, tomloc} = Toml.decode_file(tomloc_file)
 
-  @impl true
-  def handle_call(:get_table, _from, table) do
-    {:reply, table, table}
-  end
+          process_tomloc(table, basename, Map.to_list(tomloc))
+        end)
 
-  # Client
-  def start_link(params \\ []) when is_list(params) do
-    GenServer.start_link(__MODULE__, params)
-  end
+        {:noreply, table, :hibernate}
+      end
 
-  def get(loc_id, lang, params \\ [])
-      when (is_binary(loc_id) or is_atom(loc_id)) and (is_binary(lang) or is_atom(lang)) do
-    table = GenServer.call(__MODULE__, :get_table)
+      @impl true
+      def handle_call(:get_table, _from, table) do
+        {:reply, table, table}
+      end
 
-    ets_id = "#{loc_id}_#{lang}"
+      # Client
+      def start_link(params \\ []) when is_list(params) do
+        GenServer.start_link(__MODULE__, params)
+      end
 
-    case :ets.lookup_element(table, ets_id, 2) do
-      {:plain, str} -> {:ok, str}
-      {:interpolated, interpolated_str} -> Core.format_interpolated_str(interpolated_str, params)
-      {:list, list} -> {:ok, Enum.map(list, &process_list_el(&1, params))}
-    end
-  end
+      def get(loc_id, lang, params \\ [])
+          when (is_binary(loc_id) or is_atom(loc_id)) and (is_binary(lang) or is_atom(lang)) do
+        table = GenServer.call(__MODULE__, :get_table)
 
-  defp process_list_el({:plain, str}, _), do: str
+        ets_id = "#{loc_id}_#{lang}"
 
-  defp process_list_el({:interpolated, interpolated_str}, params) do
-    {:ok, str} = Core.format_interpolated_str(interpolated_str, params)
-    str
-  end
-
-  defp process_tomloc(_table, _basename, []), do: :noop
-
-  defp process_tomloc(table, basename, [{loc_id, langs} | tail])
-       when is_binary(loc_id) and is_map(langs) do
-    Enum.each(langs, fn {lang, transl} ->
-      ets_id = "#{basename}_#{loc_id}_#{lang}"
-
-      ets_record =
-        if is_list(transl) do
-          {:list, Enum.map(transl, &StringParser.parse(&1))}
-        else
-          StringParser.parse(transl)
+        case :ets.lookup_element(table, ets_id, 2) do
+          {:plain, str} -> {:ok, str}
+          {:interpolated, interpolated_str} -> Core.format_interpolated_str(interpolated_str, params)
+          {:list, list} -> {:ok, Enum.map(list, &process_list_el(&1, params))}
         end
+      end
 
-      insert(ets_id, ets_record, table)
-    end)
+      defp process_list_el({:plain, str}, _), do: str
 
-    process_tomloc(table, basename, tail)
-  end
+      defp process_list_el({:interpolated, interpolated_str}, params) do
+        {:ok, str} = Core.format_interpolated_str(interpolated_str, params)
+        str
+      end
 
-  defp process_tomloc(table, basename, [_head | tail]), do: process_tomloc(table, basename, tail)
+      defp process_tomloc(_table, _basename, []), do: :noop
 
-  defp insert(ets_id, ets_record, ets_table) when is_tuple(ets_record) do
-    true = :ets.insert_new(ets_table, {ets_id, ets_record})
+      defp process_tomloc(table, basename, [{loc_id, langs} | tail])
+           when is_binary(loc_id) and is_map(langs) do
+        Enum.each(langs, fn {lang, transl} ->
+          ets_id = "#{basename}_#{loc_id}_#{lang}"
+
+          ets_record =
+            if is_list(transl) do
+              {:list, Enum.map(transl, &StringParser.parse(&1))}
+            else
+              StringParser.parse(transl)
+            end
+
+          insert(ets_id, ets_record, table)
+        end)
+
+        process_tomloc(table, basename, tail)
+      end
+
+      defp process_tomloc(table, basename, [_head | tail]), do: process_tomloc(table, basename, tail)
+
+      defp insert(ets_id, ets_record, ets_table) when is_tuple(ets_record) do
+        true = :ets.insert_new(ets_table, {ets_id, ets_record})
+      end
+    end
   end
 end
--- test/tomloc_test.exs
+++ test/tomloc_test.exs
@@ -1,27 +1,32 @@
 defmodule TomlocTest do
   @moduledoc false
 
+  defmodule TomlocTester do
+    @moduledoc false
+    use Tomloc, otp_app: :tomloc
+  end
+
   use ExUnit.Case
   doctest Tomloc
 
   setup do
-    {:ok, _} = start_supervised(Tomloc)
+    {:ok, _} = start_supervised(TomlocTester)
     :ok
   end
 
   test "can return string with string key" do
-    assert {:ok, "Привіт світ"} == Tomloc.get("example_hello", :uk)
+    assert {:ok, "Привіт світ"} == TomlocTester.get("example_hello", :uk)
   end
 
   test "can return string with atom key" do
-    assert {:ok, "Bye Bye"} == Tomloc.get(:example_bye_bye, :en)
+    assert {:ok, "Bye Bye"} == TomlocTester.get(:example_bye_bye, :en)
   end
 
   test "can return parameterized string" do
-    assert {:ok, "Привіт, незнайомцю!"} == Tomloc.get(:example_param, :uk, name: "незнайомцю")
+    assert {:ok, "Привіт, незнайомцю!"} == TomlocTester.get(:example_param, :uk, name: "незнайомцю")
   end
 
   test "can return string lists" do
-    assert {:ok, ["one", "2", "three"]} = Tomloc.get(:example_list, :en, two: 2)
+    assert {:ok, ["one", "2", "three"]} = TomlocTester.get(:example_list, :en, two: 2)
   end
 end
