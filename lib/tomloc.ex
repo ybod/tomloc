c193c04c0ea3102dd53c7eb6445e7b83a33bf2e3
ybod <yuriibodarev@gmail.com>
2019-02-21 15:41:31 +0200

    Implement Tomlock GenServer async init with handle_continue callback

--- lib/tomloc.ex
+++ lib/tomloc.ex
@@ -17,16 +17,13 @@ defmodule Tomloc do
       # Server (callbacks)
       @impl true
       def init(params) do
-        table = :ets.new(__MODULE__, [:set, :protected, {:read_concurrency, true}])
+        ets_table = :ets.new(__MODULE__, [:set, :protected, {:read_concurrency, true}])
 
-        send(self(), {:init, table, params})
-        Process.register(self(), __MODULE__)
-
-        {:ok, table}
+        {:ok, ets_table, {:continue, params}}
       end
 
       @impl true
-      def handle_info({:init, table, params}, _) do
+      def handle_continue(params, ets_table) do
         tomloc_dir = Keyword.get(params, :localizations_dir) || Application.get_env(:tomloc, :tomloc_dir, "tomloc")
 
         tomlock_files =
@@ -38,38 +35,38 @@ defmodule Tomloc do
           basename = Path.basename(tomloc_file, ".toml")
           {:ok, tomloc} = Toml.decode_file(tomloc_file)
 
-          process_tomloc(table, basename, Map.to_list(tomloc))
+          process_tomloc(ets_table, basename, Map.to_list(tomloc))
         end)
 
-        {:noreply, table, :hibernate}
+        {:noreply, ets_table, :hibernate}
       end
 
       @impl true
-      def handle_call(:get_table, _from, table) do
-        {:reply, table, table}
+      def handle_call(:get_table, _from, ets_table) do
+        {:reply, ets_table, ets_table}
       end
 
       # Client
       def start_link(params \\ []) when is_list(params) do
-        GenServer.start_link(__MODULE__, params)
+        GenServer.start_link(__MODULE__, params, name: __MODULE__)
       end
 
       def get(loc_id, lang, params \\ [])
           when (is_binary(loc_id) or is_atom(loc_id)) and (is_binary(lang) or is_atom(lang)) do
-        table = GenServer.call(__MODULE__, :get_table)
+        ets_table = GenServer.call(__MODULE__, :get_table)
 
         ets_id = "#{loc_id}_#{lang}"
         fallback_id = "#{loc_id}_#{@fallback_lang}"
 
         cond do
-          :ets.member(table, ets_id) -> get_translaction(table, ets_id, params)
-          :ets.member(table, fallback_id) -> get_translaction(table, fallback_id, params)
+          :ets.member(ets_table, ets_id) -> get_translaction(ets_table, ets_id, params)
+          :ets.member(ets_table, fallback_id) -> get_translaction(ets_table, fallback_id, params)
           true -> {:error, {:translation, "not found: id #{loc_id}, lang #{lang}, fallback lang #{@fallback_lang}"}}
         end
       end
 
-      defp get_translaction(table, ets_id, params) do
-        case :ets.lookup_element(table, ets_id, 2) do
+      defp get_translaction(ets_table, ets_id, params) do
+        case :ets.lookup_element(ets_table, ets_id, 2) do
           {:plain, str} -> {:ok, str}
           {:interpolated, interpolated_str} -> Core.format_interpolated_str(interpolated_str, params)
           {:list, list} -> {:ok, Enum.map(list, &process_list_el(&1, params))}
@@ -91,7 +88,7 @@ defmodule Tomloc do
 
       defp process_tomloc(_table, _basename, []), do: :noop
 
-      defp process_tomloc(table, basename, [{loc_id, langs} | tail])
+      defp process_tomloc(ets_table, basename, [{loc_id, langs} | tail])
            when is_binary(loc_id) and is_map(langs) do
         Enum.each(langs, fn {lang, transl} ->
           ets_id = "#{basename}_#{loc_id}_#{lang}"
@@ -103,13 +100,13 @@ defmodule Tomloc do
               StringParser.parse(transl)
             end
 
-          insert(ets_id, ets_record, table)
+          insert(ets_id, ets_record, ets_table)
         end)
 
-        process_tomloc(table, basename, tail)
+        process_tomloc(ets_table, basename, tail)
       end
 
-      defp process_tomloc(table, basename, [_head | tail]), do: process_tomloc(table, basename, tail)
+      defp process_tomloc(ets_table, basename, [_head | tail]), do: process_tomloc(ets_table, basename, tail)
 
       defp insert(ets_id, ets_record, ets_table) when is_tuple(ets_record) do
         true = :ets.insert_new(ets_table, {ets_id, ets_record})
